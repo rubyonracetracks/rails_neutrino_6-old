68baf6157ac97b4d701d76fbe88f324a4f8af228
Jason Hsu <jhsu802701@jasonhsu.com>
2021-04-17 03:40:48 +0000

    Updated Git comments in 01-01

--- mod/mod-01-01/mod-01-01.sh
+++ mod/mod-01-01/mod-01-01.sh
@@ -13,11 +13,11 @@ echo '###################'
 echo 'UNIT 1: BASIC SETUP'
 echo '###################'
 
-echo '################################'
-echo 'Unit 1 Chapter 1: Initial Commit'
-echo '################################'
+echo '###########################'
+echo 'Unit 1 Chapter 1: GITIGNORE'
+echo '###########################'
 
 ruby mod-01-01.rb
-git init
+
 git add .
-git commit -m "Initial commit"
+git commit -m "Updated .gitignore"
