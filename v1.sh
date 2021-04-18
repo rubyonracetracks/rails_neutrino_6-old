0d7df7d5cb852500944c48d443b07eb0b9881ac7
Jason Hsu <jhsu802701@jasonhsu.com>
2021-04-18 05:00:23 +0000

    short.sh -> v1.sh; build-rails uses variables stored in tmp/*.txt files

--- v1.sh
+++ v1.sh
@@ -10,7 +10,17 @@
 
 bash credentials.sh
 
+mkdir -p tmp
+echo 'N' > tmp/host_env.txt
+echo 'Y' > tmp/from_scratch.txt
+echo 'N' > tmp/dockerize.txt
+echo 'N' > tmp/add_lint.txt
+echo 'N' > tmp/add_vulnerability_tests.txt
+echo 'N' > tmp/add_other.txt
+echo 'N' > tmp/annotate.txt
+
 DATE=`date -u +%Y%m%d-%H%M%S-%3N`
+APP_NAME="railv1-$DATE"
 
 mkdir -p log
-$PWD/build-rails 'short' "$DATE" 2>&1 | tee log/short-$DATE.txt
+$PWD/build-rails "$APP_NAME" 2>&1 | tee log/short-$DATE.txt
